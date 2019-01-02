60875fd624c68e055372a2aa1fd35658fe548a77
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-01-02 10:51:34 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int a;
+    int b;
+    int c;
+    int d;
+    cout << "Hello user! 4 numbers will appear, please type 1-4 to start a story or something, whatever I code is what you do.\n" << endl;
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+    cout << "For Story a, type 1 \n" << endl;
 }
